299cf27948ba577fd380b07a8b29ea794d5d079c
rivalcoba <ivan.rivalcoba@gmail.com>
2022-10-21 08:07:56 -0500

    updating index

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,16 +1,6 @@
 <script lang="ts" setup>
-const query = ref('');
-
-const search = () => {
-  $fetch(`http://www.omdbapi.com/?apikey=a755b202&s=${query.value}`);
-};
 </script>
 
-<template>
-  <form @submit.prevent="search">
-    <input type="text" v-model="query" />
-    <button>Search</button>
-  </form>
-</template>
+<template></template>
 
 <style scoped></style>
