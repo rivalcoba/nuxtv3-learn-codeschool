0dc96c3e73a322f5e2ebc6f0a7625b40a27e1844
rivalcoba <ivan.rivalcoba@gmail.com>
2022-10-21 08:57:30 -0500

    Searching ok!!

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,16 +1,19 @@
 <script lang="ts" setup>
 const query = ref("");
 
-const search = () => {
+const search = (e: SubmitEvent) => {
   $fetch(`http://www.omdbapi.com/?apikey=a755b202&s=${query.value}`);
+  return e
 };
 </script>
 <template>
-  <div>Page: Movies 🎥</div>
-  <form @submit.prevent="search">
-    <input type="text" v-model="query" />
-    <button>Search</button>
-  </form>
+  <div class="">
+    <div>Page: Movies 🎥</div>
+    <form @submit.prevent="search">
+      <input type="text" v-model="query" />
+      <button>Search</button>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
